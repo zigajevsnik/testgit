a32551abb2eb382cda7ab2035fe2282905a91951
zigajevsnik <ziga.jevsnik@student.um.si>
2018-11-22 10:22:08 +0100

    sporocilo

--- hello.cs
+++ hello.cs
@@ -4,10 +4,7 @@ public class Hello
    public static void Main()
    {
 	System.Console.WriteLine("Hello, World!");
+	System.Console.WriteLine("Hello, GitHub!");
 	System.Console.WriteLine("Hello, Local!");
    }
 }
-<<<<<<< HEAD:new 1.cs
-}
-=======
->>>>>>> 39f82d9a4d0956f035c1d92c89feb98edbf28f1b:hello.cs
